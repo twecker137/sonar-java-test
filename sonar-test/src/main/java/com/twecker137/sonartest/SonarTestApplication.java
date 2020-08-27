a07dd112a17b6d301ba141281cd080568db84b34
Tony Wecker <26899356+twecker137@users.noreply.github.com>
2020-08-27 09:40:47 +0200

    added springboot example

---
+++ sonar-test/src/main/java/com/twecker137/sonartest/SonarTestApplication.java
@@ -0,0 +1,13 @@
+package com.twecker137.sonartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarTestApplication.class, args);
+	}
+
+}
---
+++ sonar-test/src/test/java/com/twecker137/sonartest/SonarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twecker137.sonartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
